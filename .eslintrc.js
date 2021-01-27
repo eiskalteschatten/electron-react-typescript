dd27a049149eefa0af38a111a3ca2f2c6d80383b
Alex <alex@alexseifert.com>
2021-01-27 22:14:29 +0100

    Update ECMA version in linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
-    'ecmaVersion': 2018,
+    'ecmaVersion': 2020,
     'sourceType': 'module'
   },
   'plugins': [
@@ -52,7 +52,7 @@ module.exports = {
     'rules': {
       'quotes': ['error', 'double'],
       'semi': ['error', 'never']
-    }
+    },
   },
   {
     'files': ['*.js'],
