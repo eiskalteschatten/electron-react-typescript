52bdf3b741125417abc7a7ce38ce0332ba70601e
Alex Seifert <alex@alexseifert.com>
2021-11-14 14:24:50 +0100

    Use the default locale from the config

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 
 import { IntlProviderWrapper } from './intl/IntlContext';
 import MainLayout from './components/MainLayout';
+import config from './config';
 
 const App: React.FC = () => {
   // TODO: allow the saved locale from the DB to override the system's settings
-  const [locale] = useState<string>(navigator.language.split('-')[0] || 'en');
+  const [locale] = useState<string>(navigator.language.split('-')[0] || config.intl.defaultLocale);
 
   return (
     <>
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,7 @@ export default {
   updates: {
     url: 'update url',
   },
+  intl: {
+    defaultLocale: 'en',
+  },
 };
