31e3238bdf3437964dff1383560e0abdadf51f42
Alex Seifert <alex@alexseifert.com>
2021-11-24 10:24:31 +0100

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainLayout from './components/MainLayout';
 
 const App: React.FC = () => {
   return (
-    <>
-      <MainLayout />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<MainLayout />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
