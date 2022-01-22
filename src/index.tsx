80e1fe1aa204d149f106ed98114e35a85d28b643
Alex Seifert <alex@alexseifert.com>
2022-01-22 22:29:22 +0100

    Remove Bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import 'bootstrap/scss/bootstrap.scss';
 
 import './i18n';
 import App from './App';
@@ -9,7 +8,8 @@ import store from './store';
 import * as serviceWorker from './serviceWorker';
 import './eventsFromMain';
 
-import './index.scss';
+import './assets/css/reboot.css';
+import './scss/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
