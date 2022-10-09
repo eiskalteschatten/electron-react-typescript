8d62724a1076d3efe91d47baf03978d658bcbe8d
Alex Seifert <alex@alexseifert.com>
2022-10-09 16:37:36 +0200

    Correct the global window type definition

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,7 @@ declare global {
   interface Window {
     api: {
       send: (channel: any, ...data: any[]) => void;
+      sendSync: (channel: any, ...data: any[]) => void;
       on: (channel: any, callback: any) => void;
       once: (channel: any, callback: any) => void;
       removeListener: (channel: any, callback: any) => void;
