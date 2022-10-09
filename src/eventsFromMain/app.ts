6c922d3f8784dc2b0ca41b122a7b5208bc4a1e41
Alex Seifert <alex@alexseifert.com>
2022-10-09 22:17:00 +0200

    Cleanup

--- src/eventsFromMain/app.ts
+++ src/eventsFromMain/app.ts
@@ -3,6 +3,7 @@ import { IpcRendererEvent } from 'electron';
 import { dispatch } from '../store';
 import { setPlatform } from '../store/entities/app';
 
-window.api.on('setPlatform', (e: IpcRendererEvent, platform: string): any =>
-  dispatch(setPlatform(platform)));
+window.api.on('setPlatform', (e: IpcRendererEvent, platform: string) => {
+  dispatch(setPlatform(platform));
+});
 
