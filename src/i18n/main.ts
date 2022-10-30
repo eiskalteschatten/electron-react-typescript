541493be2eb125927f1df568437a4473d9ff94de
Alex Seifert <alex@alexseifert.com>
2022-10-30 22:17:37 +0100

    Don't initialize react-i18next in the main process

--- src/i18n/main.ts
+++ src/i18n/main.ts
@@ -1,12 +1,10 @@
 import { app } from 'electron';
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 
 import defaulti18nConfig from './config';
 
 export const setupi18n = (): void => {
   i18n
-    .use(initReactI18next)
     .init({
       ...defaulti18nConfig,
       lng: app.getLocale(),
