01c521af295f06cb96e2eef76f018a9855745528
Alex Seifert <alex@alexseifert.com>
2022-10-30 22:19:39 +0100

    Uninstall react-i18next

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,9 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 
 import config from '../config';
 import defaulti18nConfig from './config';
 
 i18n
-  .use(initReactI18next)
   .init({
     ...defaulti18nConfig,
     lng: navigator.language.split('-')[0] || config.intl.defaultLocale,
