e35c3c233fc762dfc925688750ec18122e47491a
Alex Seifert <alex@alexseifert.com>
2023-02-04 10:35:30 +0100

    Update packages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,10 +3,17 @@ import i18n from 'i18next';
 import config from '../config';
 import defaulti18nConfig from './config';
 
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    returnNull: false
+  }
+}
+
 i18n
   .init({
     ...defaulti18nConfig,
     lng: navigator.language.split('-')[0] || config.intl.defaultLocale,
+    returnNull: false,
   });
 
 export default i18n;
